53bc6aa5212eccf4481fb0482f6473b469d17806
spartajet <spartajet.guo@gmail.com>
2017-08-09 09:57:32 +0200

    complete fragment view pager

---
+++ app/src/main/java/com/ptb/length/involutecalculator/InvolutePageAdapter.java
@@ -0,0 +1,35 @@
+package com.ptb.length.involutecalculator;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by spartajet on 2017/8/9.
+ */
+
+public class InvolutePageAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragments = new ArrayList<>();
+
+    public InvolutePageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+    public void addFragment(Fragment fragment) {
+        mFragments.add(fragment);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
--- app/src/main/java/com/ptb/length/involutecalculator/MainActivity.java
+++ app/src/main/java/com/ptb/length/involutecalculator/MainActivity.java
@@ -1,23 +1,23 @@
 package com.ptb.length.involutecalculator;
 
-import android.support.v4.view.PagerAdapter;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ContradictionFragment.OnFragmentInteractionListener,ValueFixedFragment.OnFragmentInteractionListener {
     private ViewPager viewPager;
     private List<View> mViewList = new ArrayList<>();
     private LayoutInflater inflater;
-    private View valueFixedView;
-    private View contradictionView;
+
+    private ValueFixedFragment valueFixedFragment;
+    private ContradictionFragment contradictionFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,39 +27,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        this.inflater = getLayoutInflater();
-        this.valueFixedView = inflater.inflate(R.layout.value_fixed_activity, null);
-        this.contradictionView = inflater.inflate(R.layout.contradiction_activity, null);
-        this.mViewList.add(valueFixedView);
-        this.mViewList.add(contradictionView);
+        this.valueFixedFragment = new ValueFixedFragment();
+        this.contradictionFragment = new ContradictionFragment();
         this.viewPager = (ViewPager) findViewById(R.id.viewpager);
-        this.viewPager.setAdapter(new MyPagerAdapter());
-        this.viewPager.setCurrentItem(0);
+        InvolutePageAdapter pagerAdapter = new InvolutePageAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(valueFixedFragment);
+        pagerAdapter.addFragment(contradictionFragment);
+        viewPager.setAdapter(pagerAdapter);
     }
 
 
-    private class MyPagerAdapter extends PagerAdapter {
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        @Override
-        public int getCount() {
-            return mViewList.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(mViewList.get(position));
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(mViewList.get(position), 0);
-            return mViewList.get(position);
-        }
     }
-
 }
